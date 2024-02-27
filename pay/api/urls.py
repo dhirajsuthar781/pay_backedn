26719aa318f5c81fd3f5b8b381a01e1a5240345c
dhirajsuthar781 <146662607+dhirajsuthar781@users.noreply.github.com>
2024-02-27 21:39:53 +0530

    Delete pay directory

--- pay/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('addinquiry/', views.upload_inquiry, name='upload_inquiry')
-]
